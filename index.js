56e9a48cfb0ab024cdbb6b0c311a79c832f6a4ac
rashedrk <rashedrk001@gmail.com>
2023-04-13 10:17:19 +0600

    server

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const phone = require('./phone.json')
 
 app.get('/',(req,res) => {
     res.send('phone data is coming soon.......')
+})
+
+app.get('/phone', (req, res) => {
+    res.send(phone)
+})
+
+app.get('/phone/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    res.send(phone.find(phone => phone.id === id) || {})
 })
 
 app.listen(port, () => {
